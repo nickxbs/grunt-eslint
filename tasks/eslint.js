0efe8aa27acb4cb165427204d247cdb6baeec136
Nicola Febbrari <febbrari@gmail.com>
2016-07-07 14:44:33 +0200

    fix eslint.js

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const eslint = require('eslint');
 
 module.exports = grunt => {
-	grunt.registerMultiTask('eslint', 'Validate files with ESLint', function () {
+	grunt.registerMultiTask('eslint', 'Validate files with ESLint', function() {
 		const opts = this.options({
 			outputFile: false,
 			quiet: false,
@@ -53,24 +53,24 @@ module.exports = grunt => {
 
 		const tooManyWarnings = opts.maxWarnings >= 0 && report.warningCount > opts.maxWarnings;
 		const tooManyErrors = opts.maxErrors >= 0 && report.errorCount > opts.maxErrors;
-		
+
 		if (!tooManyErrors && tooManyWarnings) {
 			grunt.warn(`ESLint found too many warnings (maximum: ${opts.maxWarnings})`);
 			return false;
 		}
-		
+
 		if (tooManyErrors) {
 			grunt.log.writeln(chalk.red(`ESLint found too many  errors: ${report.errorCount}(maximum: ${opts.maxErrors})`));
 			return false;
-		} 						
-		if (report.warningCount>0) {
-			grunt.log.ok(`ESLint found some warning: ${report.warningCount}(maximum: ${opts.maxWarnings})`);			
-		} 		
-		
-		if (report.errorCount>0) 
-			grunt.log.ok(`ESLint found some error: ${report.errorCount}(maximum: ${opts.maxErrors})`);			
-		} 		
-		
+		}
+		if (report.warningCount > 0) {
+			grunt.log.ok(`ESLint found some warning: ${report.warningCount}(maximum: ${opts.maxWarnings})`);
+		}
+
+		if (report.errorCount > 0) {
+			grunt.log.ok(`ESLint found some error: ${report.errorCount}(maximum: ${opts.maxErrors})`);
+		}
+
 		return true;
 	});
 };
